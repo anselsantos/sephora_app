7d157997e64cd0b7952ed361634a200c3e4f1458
spoonfork <33741111+anselsantos@users.noreply.github.com>
2018-02-12 21:54:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,8 @@ def startpage():
 	return render_template('startpage.html')
 
 @app.route('/skincare')
-def index(chartID = 'chart_ID', chart_type = 'bar', chart_height = 350):
-	chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
-	series = [{"name": 'Label1', "data": [1,2,3]}, {"name": 'Label2', "data": [4, 5, 6]}]
-	title = {"text": 'My Title'}
-	xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
-	yAxis = {"title": {"text": 'yAxis Label'}}
-	return render_template('skincare.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
+def skincare():
+	return render_template('skincare.html')
 
 @app.route('/bathandbody')
 def bathandbody():
@@ -146,4 +141,4 @@ def results():
 
 
 if __name__ == "__main__":
-	app.run(debug = True, host='0.0.0.0', port=8081, passthrough_errors=True)
+	app.run()
